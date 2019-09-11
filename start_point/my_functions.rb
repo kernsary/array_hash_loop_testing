76378e7383c7df1c22bd125f7f81c9fe71d59c75
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-09-11 16:40:31 +0100

    initial commit

---
+++ start_point/my_functions.rb
@@ -0,0 +1,31 @@
+#def add_array_lengths( array1, array2)
+#  return array1.length + array2.length
+#end
+
+#------------------------------------------------
+
+# def sum_array( numbers )
+#   total = 0
+#   for number in numbers
+#     total += number
+#   end
+#   return total
+# end
+
+#------------------------------------------------
+
+# def is_item_in_array( array, item_guessed )
+#   in_array = false
+#   for item in array
+#     if item_guessed == item
+#       in_array = true
+#     end
+#   end
+#   return in_array
+# end
+
+#-------------------------------------------------
+
+def get_first_key(hash)
+  return hash.keys[0]
+end
